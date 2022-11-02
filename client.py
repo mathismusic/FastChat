86396b85e45202d92c746b4345275f54c704daf6
mathismusic <krishna.agaram1729@gmail.com>
2022-11-02 14:32:22 +0530

    updated files

--- client.py
+++ client.py
@@ -3,29 +3,10 @@
 import socket
 import json
 import sys
-import asyncio
 from select import select
 
 HOST = "192.168.103.215"  # The server's hostname or IP address
 PORT = 61002  # The port used by the server
-
-async def user_input():
-    while True:
-        loop = asyncio.get_event_loop()
-        receiver = await loop.run_in_executor(None, input, "To: ")
-        message = input("Message: ")
-        s.sendall(bytes(str(Message(username,receiver,message)).encode()))
-
-async def print_something():
-    await asyncio.sleep(180)
-    data = s.recv(1024).decode()
-    if data != "received": print("Received:", data)
-    else: print("Message sent successfully.") # tick.
-
-async def main():
-    tasks = [user_input(), print_something()]
-    await asyncio.gather(*tasks)
-
 
 class Message:
     def __init__(self, sender, rec, msg) -> None:
@@ -35,6 +16,11 @@ class Message:
     
     def __repr__(self) -> str:
         return json.dumps({"Sender": self.sender, "Recipient": self.recipient, "Message": self.message})
+
+class Client:
+    def __init__(self) -> None:
+        pass
+    def login(self) -> None:
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
