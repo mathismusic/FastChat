55c15d1717d374df74208e49071fca1717eb77eb
210070007 <210070007@iitb.ac.in>
2022-11-22 18:48:18 +0530

    modfied message class to carry fernet key

--- client.py
+++ client.py
@@ -12,13 +12,14 @@ class Message:
     This is the standard format of all messages transferred between servers and clients.
     It is in standard JSON format with attributes Sender, Recipient and Message
     """
-    def __init__(self, sender, rec, msg) -> None:
+    def __init__(self, sender, rec, msg, key) -> None:
         self.sender =  sender
         self.recipient = rec
         self.message = msg
+        self.fernet_key = key
     
     def __repr__(self) -> str:
-        return json.dumps({"Sender": self.sender, "Recipient": self.recipient, "Message": self.message})
+        return json.dumps({"Sender": self.sender, "Recipient": self.recipient, "Message": self.message, "Key": self.fernet_key })
 
 class Client:
     """
