a920ff6442f634eae5835b56d934e1fdd02b9212
210070007 <210070007@iitb.ac.in>
2022-11-24 16:06:04 +0530

    added group parameter in message

--- message.py
+++ message.py
@@ -5,11 +5,12 @@ class Message:
     This is the standard format of all messages transferred between servers and clients.
     It is in standard JSON format with attributes Sender, Recipient and Message
     """
-    def __init__(self, sender, rec, msg, key) -> None:
+    def __init__(self, sender, rec, msg, key, grp_name =  None) -> None:
         self.sender =  sender
         self.recipient = rec
         self.message = msg
         self.fernet_key = key
+        self.group_name = grp_name
     
     def __repr__(self) -> str:
-        return json.dumps({"Sender": self.sender, "Recipient": self.recipient, "Message": self.message, "Key": self.fernet_key })
+        return json.dumps({"Sender": self.sender, "Recipient": self.recipient, "Message": self.message, "Key": self.fernet_key, "Group_Name": self.group_name })
